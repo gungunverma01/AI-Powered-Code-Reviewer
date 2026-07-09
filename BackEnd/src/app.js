c6a7874dc3c43ad58597a6bf08f16852fb2151b3
Gungun Verma <gungunverma670@gmail.com>
2026-07-09 16:39:32 +0530

    update the server.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/ai", aiRoutes);
-
-// Serve React build
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
+
+app.use("/ai", aiRoutes);
 
 module.exports = app;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,9 +24,10 @@ function App() {
 
   async function reviewCode() {
   try {
-    const response = await axios.post("/ai/get-review", {
-  code,
-});
+    const response = await axios.post(
+  "http://localhost:3000/ai/get-review",
+  { code }
+);
 
     setReview(response.data);
   } catch (err) {
